6cbb58b631dcbd911969b61e4121da6be1342006
Qihong Lu <7212521+qihongl@users.noreply.github.com>
2023-01-03 02:40:49 -0500

    Create submit-train.sh

---
+++ src/submit-train.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+dim_hidden=16
+ctx_wt=.5
+
+
+for subj_id in {0..2}
+do
+   for lr in 1e-3 1e-4
+   do
+       for update_freq in 2 4 8
+       do
+           for dim_context in 64 256
+           do
+               for penalty_new_context in .1 .5 1
+               do
+                   for stickiness in .1 .5 1
+                   do
+                       sbatch train.sh
+                           --${subj_id} \
+                           --${lr} \
+                           --${update_freq} \
+                           --${dim_hidden} \
+                           --${dim_context} \
+                           --${ctx_wt} \
+                           --${penalty_new_context} \
+                           --${stickiness}
+                   done
+               done
+           done
+       done
+   done
+done
+
+
