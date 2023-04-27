b01844e09e2cdd1d28dc18218c6d7d9162429e64
Qihong Lu <7212521+qihongl@users.noreply.github.com>
2023-04-27 13:58:48 -0400

    eval

--- src/eval-group.sh
+++ src/eval-group.sh
@@ -8,7 +8,8 @@
 
 LOGROOT=/tigress/qlu/logs/meta-model/log
 # LOGROOT=/scratch/gpfs/qlu/logs/meta-model/log
-DT=2023-04-19
+# DT=2023-04-19
+DT=new-mem
 
 echo $(date)
 
--- src/submit-eval-group.sh
+++ src/submit-eval-group.sh
@@ -18,7 +18,7 @@ do
             for try_reset_h in 0
             do
               use_shortcut=1
-              for gen_grad in .1 .5 1 3
+              for gen_grad in .1 .5 3
                 do
                   for pe_tracker_size in 256
                   do
