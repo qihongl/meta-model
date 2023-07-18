868a79b760d5a5e3d66c71b1e8572ce8b516198d
Qihong Lu <7212521+qihongl@users.noreply.github.com>
2023-07-17 22:44:16 -0400

    Update train.sh

--- src/train.sh
+++ src/train.sh
@@ -9,8 +9,8 @@
 LOGROOT=/tigress/qlu/logs/meta-model/log
 # LOGROOT=/scratch/gpfs/qlu/logs/meta-model/log
 
-DT=$(date +%Y-%m-%d)
-# DT=new-mem
+# DT=$(date +%Y-%m-%d)
+DT=freeze-ctx-test
 
 echo $(date)
 
